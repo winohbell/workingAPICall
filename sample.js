627f1e29d540297f5f56fdc0f6753147fd521888
Tanzhi Liu <winohbell@yahoo.com>
2020-10-01 18:18:23 -0400

    ok

---
+++ sample.js
@@ -0,0 +1,18 @@
+// server file: https://github.com/HRR48-FEC-SquawkinPhoenix/Seller/blob/master/server/index.js
+// API request to get first seller
+// http request: http://localhost:3004/api/seller/1
+
+[
+  {
+      "id": 1,
+      "sellerName": "RayBailey",
+      "storeName": "Rippin, Steuber and Lynch",
+      "shippingSpeedSlowest": 4,
+      "shippingSpeedFastest": 9,
+      "sellerLocation": "West Darron, Minnesota",
+      "exchangePossibility": 1,
+      "returnCancelPossibility": 1,
+      "createdAt": "2020-10-01T16:27:44.000Z",
+      "updatedAt": "2020-10-01T16:27:44.000Z"
+  }
+]
